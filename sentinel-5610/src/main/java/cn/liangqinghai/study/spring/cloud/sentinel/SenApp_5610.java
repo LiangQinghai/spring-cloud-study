253222721a1241e9fcbf634d0c7048e54361b02a
liangqinghai <liangqinghai@wakedata.com>
2020-08-06 23:54:42 +0800

    sentinel

---
+++ sentinel-5610/src/main/java/cn/liangqinghai/study/spring/cloud/sentinel/SenApp_5610.java
@@ -0,0 +1,17 @@
+package cn.liangqinghai.study.spring.cloud.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SenApp_5610 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SenApp_5610.class, args);
+
+    }
+
+}
